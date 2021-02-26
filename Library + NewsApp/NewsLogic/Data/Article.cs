348acf2a12e809f162afd91e0995b6a01cd11d85
taperte <jana.taperte@gmail.com>
2021-02-26 02:15:07 +0200

    ..

--- Library + NewsApp/NewsLogic/Data/Article.cs
+++ Library + NewsApp/NewsLogic/Data/Article.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace NewsLogic.Data
 {
+    public enum Topic 
+    { 
+        Latvijā,
+        Ārvalstīs,
+        Kultūra,
+        Kriminālziņas,
+        Sports,
+        Auto
+    }
     public class Article
     {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Time { get; set; }
+        public string Body { get; set; }
+        public Topic Topic { get; set; }
     }
 }
