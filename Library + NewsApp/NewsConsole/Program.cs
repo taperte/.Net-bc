59f7d4355d72a6c0cb22ca782148850cefb06951
taperte <jana.taperte@gmail.com>
2021-02-28 23:28:27 +0200

    ..

--- Library + NewsApp/NewsConsole/Program.cs
+++ Library + NewsApp/NewsConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NewsLogic.Data;
 using NewsLogic.Managers;
+using System.Linq;
 
 namespace NewsConsole
 {
@@ -9,13 +10,48 @@ namespace NewsConsole
         static void Main(string[] args)
         {
             NewsManager editor = new NewsManager();
+
             editor.PrintNews(editor.GetLatestNews(), "Latest news:");
-            editor.PrintNews(editor.GetNewsByTopic((int)TopicName.Latvia), $"{TopicName.Latvia}:");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            int articleid = 7;
-            Console.WriteLine($"Article with ID {articleid}:");
-            Console.ResetColor();
-            editor.GetArticle(articleid).PrintArticleInfo();
+
+            while (true)
+            {
+                Console.Write("Please enter topic ID or \"stop\": ");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                if (input.ToLower() == "stop")
+                {
+                    break;
+                }
+                int topicid = int.Parse(input);
+                if (topicid < 0 || topicid > 5)
+                {
+                    Console.Write("Invalid ID! ");
+                    continue;
+                }
+                editor.PrintNews(editor.GetNewsByTopic(topicid), $"{(TopicName)topicid}:");
+            }
+
+            while (true)
+            {
+                Console.Write("Please enter article ID or \"stop\": ");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                if (input.ToLower() == "stop")
+                {
+                    break;
+                }
+                int articleid = int.Parse(input);
+                Article article = editor.GetArticle(articleid);
+                if (article == null)
+                {
+                    Console.Write("Invalid ID! ");
+                    continue;
+                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Article with ID {articleid}:");
+                Console.ResetColor();
+                article.PrintArticleInfo();
+            }
         }
     }
 }
